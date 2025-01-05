e8833bf2b002051a18cfedf781566e24ee44b2a5
Zain Gill <zaingill45@gmail.com>
2025-01-04 17:49:08 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./images/icons/favicon.ico">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <main>
+        <h1>Sign In</h1>
+        <form>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit">Sign In</button>
+        </form>
+    </main>
+</body>
+
+</html>
