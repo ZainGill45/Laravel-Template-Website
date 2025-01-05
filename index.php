40ed22b4c072646bc0511bc820fee951039a46f5
Zain Gill <zaingill45@gmail.com>
2025-01-04 18:06:45 -0700

    update

--- index.php
+++ index.php
@@ -10,16 +10,7 @@
 </head>
 
 <body>
-    <main>
-        <h1>Sign In</h1>
-        <form>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-            <button type="submit">Sign In</button>
-        </form>
-    </main>
+    <h1>Hello World!</h1>
 </body>
 
 </html>
